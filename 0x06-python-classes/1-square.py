208f861a0e7e3889c8ce1022a04d73b6f13426f0
Batunde-Dev <batunde.dev@gmail.com>
2023-04-09 08:00:56 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines a class Square"""
+
+
+class Square:
+    """Initializes fields of the class"""
+
+    def __init__(self, size):
+        """The Square __init__ method
+        Args:
+            size (int): size of the square - private
+        """
+
+        self.__size = size
